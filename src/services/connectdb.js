c770d7810304ea97e5c85de66ccef73715a20c4f
allegria25 <allegriakinzoladev@gmail.com>
2024-01-28 00:23:54 +0100

    modification

--- src/services/connectdb.js
+++ src/services/connectdb.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose")
+require("dotenv").config()
+const MONGOOSE_URL = process.env.MONGOOSE_URL
 async function connectDB (){
     try {
-        await mongoose.connect(process.env.MONGOOSE_URL)
+        await mongoose.connect(MONGOOSE_URL)
         console.log("connexion à la base des données")
     } catch (error) {
         console.error("erreur lors de la connexion à la base des données", error)
